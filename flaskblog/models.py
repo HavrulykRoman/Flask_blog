665891520b897d1391c4de724dcddbc3a8fbc40b
HavrulykRoman <79399520+HavrulykRoman@users.noreply.github.com>
2022-12-08 17:04:19 +0200

    added account (#6)

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,8 +1,12 @@
 from datetime import datetime
-from __main__ import db
+from flaskblog import db, login_manager
+from flask_login import UserMixin
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -17,7 +21,7 @@ class User(db.Model):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    data_posted = db.Column(db.DataTime, nullable=False, default=datetime.utcnow)
+    data_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
